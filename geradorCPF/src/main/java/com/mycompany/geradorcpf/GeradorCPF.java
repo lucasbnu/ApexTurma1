25b7015678faa77824c5c32da0e3237270207ec2
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-06-11 17:22:26 -0300

    Final do utilCPF

--- geradorCPF/src/main/java/com/mycompany/geradorcpf/GeradorCPF.java
+++ geradorCPF/src/main/java/com/mycompany/geradorcpf/GeradorCPF.java
@@ -5,15 +5,12 @@ public class GeradorCPF {
     public static void main(String[] args) {
          utilCPF utilitarioCPF = new utilCPF();
          
-
          if ( utilitarioCPF.validaCPF("61782997024") ){
              System.out.println("CPF Verdadeiro");    
          }else {
              System.out.println("CPF False");    
          }
          System.out.println(utilitarioCPF.geraCPF());
-         
-         
-         
+            
     }    
 }
